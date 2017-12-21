014b4a5fb5aa007aceb080256664f3aece90a746
jin.yan <jin.yan@tcl.com>
2017-12-21 12:07:01 +0800

    tb spider

--- tb.py
+++ tb.py
@@ -6,6 +6,7 @@ import os
 from bs4 import BeautifulSoup
 import json
 import urllib
+import gc
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -31,21 +32,20 @@ class tbSpider():
                      self.key)
 
     #get product_id,product_name
-    def task_insert(result,m):
+    def task_insert(i,m):
         n = 0
-        for i in result:
-            print("商品id：%s \n 名称: %s \n 价格: %s 元  \n 链接: %s" % (i[0],i[1].strip(),i[2],i[3]))
-            m.insert_into_product(i[0],i[1].strip(),i[3])
-            m.insert_into_price(i[0],i[2])
-            n=n+1
-            all_price = m.select_from_price_by_product_id(i[0])
-            old_price = 0
-            for j in all_price:
-                old_price = float(j[0])
-                break
-            print float(i[2]),old_price
-            if float(i[2]) <= old_price * 80/100:
-                m.update_from_product_by_product_id(i[0])
+        print("商品id：%s \n 名称: %s \n 价格: %s 元  \n 链接: %s" % (i[0],i[1].strip(),i[2],i[3]))
+        m.insert_into_product(i[0],i[1].strip(),i[3])
+        m.insert_into_price(i[0],i[2])
+        n=n+1
+        all_price = m.select_from_price_by_product_id(i[0])
+        old_price = 0
+        for j in all_price:
+            old_price = float(j[0])
+            break
+        print float(i[2]),old_price
+        if float(i[2]) <= old_price * 80/100:
+            m.update_from_product_by_product_id(i[0])
 
     def parse_data(self):
         pages = 20
@@ -65,7 +65,6 @@ class tbSpider():
 "电脑桌","坐具","现代简约","美式家具","北欧家具","中式家具","儿童家具","真皮沙发","布艺沙发","皮床","实木床","儿童床",
 "乳胶床垫","儿童学习桌","书架","花架","椅子","电脑椅","佛山家具","发夹","发圈","发网","头巾","头箍","假发","皮筋","耳环","丝巾扣","项链","胸针","腰带","手链","手镯","戒指","脚链","饰链","儿童饰品","手机饰品","笔类","薄","本","册","日用文具","办公设备","复印机","考勤机","办公家具","仿真花","叶","藤","仿真树木","仿真水果","光纤花","干花","盆花","盆景","耳机","耳塞","电动剃须刀","电子计算器","电子记事本","字典","电筒","电池","打火","烟具","插头，插座","稳压器","充电器","照明","灯具"," 钟表仪器","仪表","游戏机","卡激光产品门钟","门铃","防盗器","点","验钞机","定时器","指南针","电子保健品","电子配件元器件","随身听","照相器材","收音机","其他电子产品","复读机","手套","毛巾","浴巾","帽子","文胸","内衣","内裤","泳装","围巾","头巾","床上用品","坐垫","靠垫","窗帘布","鞋垫","台布","茶巾","皮带","拉链","纽扣","衬料","时尚饰品","帽子","领带","围巾","头巾","手套","袜子","针织服装","梭织服装","品牌服装","男装","女装","内衣","睡衣","浴衣","衬衣","毛衣","西服","裤子","休闲服装","婚纱","礼服","工作服","制服","袜子","锁具","指甲钳","钥匙坯","刀","剑","剪刀","美工刀","五金工具","园艺五金","刮剃刀","刀片","滑板车","门窗五金","装饰水暖","保温杯","口子杯","不锈钢厨房用具","安全扣","头盔","金属匙扣","金属架","台","测量工具","胶水制品","净水器","饮水机","榨汁机","咖啡机，豆浆机","电热壶，电热杯","电炒锅","电饭煲","湿度调节器","空气净化器","取暖电器","电吹风","吸尘器","电扇","排气扇","电熨斗","视听器材","耳机","遥控器","锂电池","镍氢电池","磁卡","IP卡","IC卡","拨号器","充电器","天线","电话机","可视电话","移动电话","手机及配件","传真机","网络通信产品","通讯","声讯系统","塑料玩具","填充玩具","绒毛玩具","电子玩具","电动玩具","玩具珠","球","礼品娃娃","玩具车","玩具枪","模型玩具","益智玩具","童车","卡通漫画海报","茶叶","调味品","糖果","饼干","炒货","酒类","饮品","小食品"]
         m = Model()
-	result = None
         result2 = []
 	for product in products:
             i = 0
@@ -73,7 +72,7 @@ class tbSpider():
                 page_num = i*60
                 url_page = "https://s.taobao.com/search?initiative_id=tbindexz_20170306&ie=utf8&spm=a21bo.2017.201856-taobao-item.2&sourceId=tb.index&search_type=item&ssid=s5-e&commend=all&imgfile=&q="+urllib.quote(product)+"&suggest=0_9&_input_charset=utf-8&s="+str(page_num)
                 #url_page = "https://www.baidu.com"
-                print url_page
+                #print url_page
                 result = self.spider.get_page(url_page)
 		
                 i = i + 1
@@ -97,6 +96,9 @@ class tbSpider():
 		        url = i["detail_url"].replace('\\u00','%')
 		        result2[3]="http:" + urllib.unquote(url)
 			self.task_insert(result2,m)
+		    del url_page
+		    del result
+		    gc.collect()
 		except:
 		    pass
 
